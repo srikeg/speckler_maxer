12593a641cd5d9108933d4ccb86954e80e3618f2
Stefanie Girod <stefanie.girod@rwth-aachen.de>
2026-05-29 14:21:34 +0200

    works better

---
+++ ml/config.py
@@ -0,0 +1,16 @@
+# Model path
+
+# MODEL_PATH = "ml/model_10E.keras"
+# MODEL_PATH = "ml/model_daten4.keras"
+# MODEL_PATH = "ml/model_daten6.keras"
+MODEL_PATH = "ml/model_daten6_newSBE.keras"
+# MODEL_PATH = "ml/model.keras"
+
+# Training data directory
+TRAININGS_DATA_DIR = "trainingsdaten6"
+
+# DIR containing the images to evaluate
+# EVAL_IMAGE_DIR = "/home/group1/Desktop/test pics3"
+
+# Class label mapping (index: class name)
+CLASS_LABELS = ["ACR", "PC", "PETG", "SBE"]
